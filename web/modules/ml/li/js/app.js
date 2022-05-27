a3c90e87de592af8990c2b3c2f3d87cd8db4df61
alex-ko1 <94445109+alex-ko1@users.noreply.github.com>
2022-05-28 00:49:08 +0300

    init-2

--- web/modules/ml/li/js/app.js
+++
@@ -1,7 +0,0 @@
-(function ($, Drupal) {
-  Drupal.behaviors.myModuleBehavior = {
-    attach: function (context, settings) {
-      $("input[data-drupal-selector='edit-phone-0-value']").inputmask("+380(99)-999-99-99");
-    }
-  };
-})(jQuery, Drupal);
